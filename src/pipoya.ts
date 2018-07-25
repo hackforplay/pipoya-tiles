7ec4704ca2aa87937d4ef8798dc3ce127fb410db
teramotodaiki <i101323@gmail.com>
2018-07-25 15:57:21 +0900

    publish

--- src/pipoya.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-import generate from ".";
-
-console.time("finish");
-
-generate("./input/base.png", "./input/setting.csv").then(json => {
-  fs.writeFileSync("./output/tileset.json", json);
-  console.timeEnd("finish");
-});
